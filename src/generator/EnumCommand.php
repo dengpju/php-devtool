d16fb1f50990ce72c462b994af2e4b84d69267bf
dengpju <dengpju@163.com>
2022-03-03 15:46:34 +0800

    更新

---
+++ src/generator/EnumCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace PHPDTool\generator;
+
+
+class EnumCommand extends GeneratorCommand
+{
+
+}
---
+++ src/generator/GeneratorCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace PHPDTool\generator;
+
+
+abstract class GeneratorCommand
+{
+    protected $input;
+
+    protected $output;
+}
